68142ff2538aab98307691b868b64ee095ef5fae
divyaguru <divyaagurumoorthy@gmail.com>
2019-11-10 00:21:46 +0530

    Added the python file

---
+++ mergeJson.py
@@ -0,0 +1,42 @@
+import json
+import re as regex
+import os
+import glob
+
+
+def filebrowser():
+    return [f for f in glob.glob("*.json")]
+    
+    
+def isDictEmpty(structure):
+    if structure:
+        #print('Structure is not empty.')
+        return False
+        
+    else:
+        #print('Structure is empty.')
+        return True
+               
+
+listOfFiles = filebrowser()
+mergedArray = {}
+
+for files in listOfFiles:
+    if regex.findall("data", files):
+        with open(files) as f:
+            data = json.load(f)
+            dictionaryKeyName = list(data)
+            
+            if isDictEmpty(mergedArray):
+                mergedArray[dictionaryKeyName[0]] = []
+            
+            for p in data[dictionaryKeyName[0]]:
+                mergedArray[dictionaryKeyName[0]].append(p)
+                #print(mergedArray)
+
+        
+print(mergedArray)
+
+
+with open('merge1.json', 'w') as outfile:
+    json.dump(mergedArray, outfile, indent=4)
